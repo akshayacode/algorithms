7b1ad06fc95517149f2ae1fd03d79f2bbe2496ca
Akshaya G R <37238867+akshayacode@users.noreply.github.com>
2020-10-28 11:18:58 +0530

    updated merge Sort in java

--- MergeSort/MergeSort.java
+++ MergeSort/MergeSort.java
@@ -1,73 +1,86 @@
-public class MyMergeSort  
-{  
-   void merge(int arr[], int beg, int mid, int end)  
-    {  
+public class MergeSort {
+   
+    void merge(int arr[], int l, int m, int r)
+    {
+        
+        int n1 = m - l + 1;
+        int n2 = r - m;
+ 
+       
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+ 
+       
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+ 
+        
+        int i = 0, j = 0;
+ 
+        
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+ 
+       
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+ 
+        
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+ 
   
-       int l = mid - beg + 1;  
-       int r = end - mid;  
-  
-       intLeftArray[] = new int [l];  
-       intRightArray[] = new int [r];  
-  
-        for (int i=0; i<l; ++i)  
-           LeftArray[i] = arr[beg + i];  
-  
-        for (int j=0; j<r; ++j)  
-           RightArray[j] = arr[mid + 1+ j];  
-  
-  
-        int i = 0, j = 0;  
-        int k = beg;  
+    void sort(int arr[], int l, int r)
+    {
+        if (l < r) {
+            
+            int m = (l + r) / 2;
+ 
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+ 
+            merge(arr, l, m, r);
+        }
+    }
+ 
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
 
-        while (i<l&&j<r)  
-        {  
-           if (LeftArray[i] <= RightArray[j])  
-           {  
-               arr[k] = LeftArray[i];  
-               i++;  
-           }  
-           else  
-           {  
-               arr[k] = RightArray[j];  
-                j++;  
-           }  
-           k++;  
-        }  
-        while (i<l)  
-        {  
-           arr[k] = LeftArray[i];  
-           i++;  
-           k++;  
-        }  
-  
-        while (j<r)  
-        {  
-          arr[k] = RightArray[j];  
-           j++;  
-           k++;  
-        }  
-    }  
-  
-    void sort(int arr[], int beg, int end)  
-    {  
-        if (beg<end)  
-        {  
-           int mid = (beg+end)/2;  
-           sort(arr, beg, mid);  
-           sort(arr , mid+1, end);  
-            merge(arr, beg, mid, end);  
-        }  
-    }  
-    public static void main(String args[])  
-    {  
-        intarr[] = {6,5,3,1,8,7,2,4};  
-        MyMergeSort ob = new MyMergeSort();  
-        ob.sort(arr, 0, arr.length-1);  
-  
-        System.out.println("\nSorted array");  
-        for(int i =0; i<arr.length;i++)  
-        {  
-            System.out.println(arr[i]+"");  
-        }  
-    }  
+    public static void main(String args[])
+    {
+        int arr[] = { 6, 5, 3, 1, 8, 7, 2, 4 };
+ 
+        System.out.println("Given Array");
+        printArray(arr);
+ 
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length - 1);
+ 
+        System.out.println("\nSorted array");
+        printArray(arr);
+    }
 }
